169d7f69c08eccb278e9a59a7529328ab4d55d2f
BEAST04289 <shaurya04289@gmail.com>
2025-12-26 00:49:35 +0530

    file input output

---
+++ day8_file_io.py
@@ -0,0 +1,4 @@
+# file input/output is a way to read from and write to files on your computer.
+# This is useful for storing data, configurations, or any information that needs to persist
+# beyond the runtime of a program.
+
